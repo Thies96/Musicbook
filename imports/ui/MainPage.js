7c3a1fc959cff260a4fb5e25f6e25d16b61235e1
Unknown <thieskornek@gmx.de>
2018-03-21 19:15:46 +0100

    MainPage
    Hab die Startseite mit dem ausführlichen Login Formular (noch ohne logik um sich tatsächlich anzumelden) gestaltet und dafür gesorgt das man zwischen login und register formular wechseln kann

--- imports/ui/MainPage.js
+++ imports/ui/MainPage.js
@@ -6,71 +6,83 @@ import { withTracker } from 'meteor/react-meteor-data';
 import { Posts } from '../api/posts.js';
 import Post from './Post.js';
 import AccountsUIWrapper from './AccountsUIWrapper';
-
 // App component - represents the whole app
-class App extends Component {
+class MainPage extends Component {
+  state = {toggle: true};
 
-  handleSubmit(event) {
-    event.preventDefault();
-
-    //find text field via react ref
-    const text = ReactDOM.findDOMNode(this.refs.newPost).value.trim();
-
-    Meteor.call('posts.insert', text);
-
-    //clear form
-    ReactDOM.findDOMNode(this.refs.newPost).value = '';
+  constructor(props) {
+    super(props);
+    var save = localStorage.getItem('toggle');
+    save == 'true' ? 
+    this.state = {toggle: true} : this.state={toggle:false};
+    this.toggleLogin = this.toggleLogin.bind(this);
   }
 
-  renderPosts() {
-    let filteredPosts= this.props.posts;
-
-    return filteredPosts.map((post) => {
-      const currentUserId = this.props.currentUser && this.props.currentUser._id;
-      const showPrivateButton = post.owner === currentUserId;
-
-      return (
-        <Post
-          key={post._id}
-          post={post}
-          showPrivateButton={showPrivateButton}
-        />
-      );
-    });
+  toggleLogin(){
+   this.setState({toggle: !this.state.toggle});
+   localStorage.setItem('toggle', !this.state.toggle);
   }
 
   render() {
     return (
       <div className="container">
         <header>
-          <h1> Starting Page </h1>
+          <h1> Startseite </h1>
 
           <AccountsUIWrapper />
-          
-          { this.props.currentUser ? 
-          <form className="new-Post" onSubmit={this.handleSubmit.bind(this)} >
-          <input className="newPost"
-            type="text"
-            ref="newPost"
-            placeholder="Type a new Post!"
-          />
-          </form> : ''
-        }
-        </header>
 
-        <ul>
-          {this.renderPosts()}
-        </ul>
+        </header>
+          { this.state.toggle ?
+          <div className="LoginContainer">
+          <div className="headline">Sign Up</div>
+          <div className="formContainer">
+            <form name="register">
+              <label htmlFor="registerEmail">E-Mail o. Benutzername
+              <input type="email" name="registerEmail"></input>
+              </label> <br></br>
+              <label htmlFor="registerPassword">Password:
+              <input type="password" name="registerPassword"></input>
+              </label>
+              <span className="LoginLink">Bereits Mitglied?
+              <button className="toggleLogin" onClick={this.toggleLogin}>
+              Login
+              </button>
+              </span>
+              <input type="submit" value="Senden"></input>
+            </form>
+          </div>
+          </div>: 
+          <div className="LoginContainer">
+          <div className="headline">Login</div>
+          <div className="formContainer">
+            <form name="login">
+              <label htmlFor="loginEmail">E-Mail o. Benutzername
+              <input type="email" name="loginEmail"></input>
+              </label> <br></br>
+              <label htmlFor="loginPassword">Password:
+              <input type="password" name="loginPassword"></input>
+              </label>
+              <span className="LoginLink">Noch kein Konto?
+              <button className="toggleRegister" onClick={this.toggleLogin}>
+              Registrieren
+              </button>
+              </span>
+              <input type="submit" value="Anmelden"></input>
+            </form>
+          </div>
+          </div>
+          }
+
       </div>
     );
   }
 }
 
 export default withTracker(() => {
-  Meteor.subscribe('posts');
+  //Meteor.subscribe('posts');
 
   return {
-    posts: Posts.find({}, { sort: { createdAt: -1 } }).fetch(),
-    currentUser: Meteor.user(),
+    /*posts: Posts.find({}, { sort: { createdAt: -1 } }).fetch(),
+    currentUser: Meteor.user(), */
   };
-})(App);
+})(MainPage); 
